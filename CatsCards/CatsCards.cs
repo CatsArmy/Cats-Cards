315a88f43b11698a269fe9f415bb8ca617c0f9ec
CatsArmy <104517593+CatsArmy@users.noreply.github.com>
2023-10-12 12:30:26 +0300

    Add files via upload

---
+++ CatsCards/CatsCards.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using InControl;
+using PlayerActionsHelper;
+using UnboundLib.Networking;
+using UnboundLib;
+
+[BepInDependency("com.willis.rounds.unbound")]
+[BepInDependency("pykess.rounds.plugins.moddingutils")]
+[BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")]
+[BepInDependency("com.CrazyCoders.Rounds.RarityBundle")]
+[BepInDependency("root.rarity.lib")]
+[BepInDependency("root.cardtheme.lib")]
+[BepInDependency("com.rounds.willuwontu.ActionHelper")]
+[BepInPlugin(ModId, ModName, Version)]
+[BepInProcess("Rounds.exe")]
+
+public class CatsCards : BaseUnityPlugin
+{
+    private const string ModId = "com.CatsCards.Cats";
+    private const string ModName = "Cats Cards";
+    public const string Version = "0.0.0";
+    internal static string modInitials = "Cats";
+    internal static AssetBundle assets;
+    internal static CatsCards instance;
+    
+    void Awake()
+    {
+        var harmony = new Harmony(ModId);
+        harmony.PatchAll();
+        instance = this;
+        assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("assetbundle", typeof(CatsCards).Assembly);
+        assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
+
+        PlayerActionManager.RegisterPlayerAction(new ActionInfo("SwitchHoldable", new KeyBindingSource(Key.LeftShift),
+            new DeviceBindingSource(InputControlType.DPadDown)));
+    }    
+
+}
---
+++ CatsCards/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace Lightsaber.Utils
+{
+    internal class Utils
+    {
+        public List<CardCategory> categoryList = new List<CardCategory>();
+
+        private CardCategory cardCategory;
+
+        private CardInfo card;
+
+        private CardInfo GetCardByName(string cardName)
+        {
+            return CardChoice.instance.cards.FirstOrDefault((CardInfo c) => c.cardName.ToLower() == cardName.ToLower());
+        }
+
+        private CardCategory GetCardCategoryByName(string category)
+        {
+            return CustomCardCategories.instance.CardCategory(category);
+        }
+
+        private void AddCardCategory(string cardName, string categories = "CardManipulation")
+        {
+            try
+            {
+                card = GetCardByName(cardName);
+                cardCategory = GetCardCategoryByName(categories);
+                categoryList.Clear();
+                categoryList.AddRange(card.categories.ToList());
+                if (!categoryList.Contains(cardCategory))
+                {
+                    categoryList.Add(cardCategory);
+                    card.categories = new CardCategory[categoryList.ToArray().Length];
+                    card.categories = categoryList.ToArray();
+                    UnityEngine.Debug.Log($"log card ||");
+                }
+                else
+                    UnityEngine.Debug.Log($"{categories} is allready present on {card.name}");
+            }
+            catch (Exception exception)
+            {
+                UnityEngine.Debug.Log(exception.Message);
+            }
+        }
+    }
+}
